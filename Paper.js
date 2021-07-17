0a84ef6675556218535e0b39dd4af2155376bdfa
TaranKumar <78540263+TaranKumar@users.noreply.github.com>
2021-07-17 17:47:49 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,32 @@
+class Paper{
+    cunstructor(x,y,r){
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction:0,
+            density: 1.2
+        }
+
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.image = loadImage("paper.png");
+        this.body = Bodies.circle(this.x,this.y,(this.r-20)/2, options);
+        World.add(world, this.body);
+
+        /*
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        */
+    }
+    display(){
+        var paperPos = this.body.position;
+        push();
+        translate(paperPos.x, paperPos.y);
+        rectMode(CENTER);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.r, this.r);
+        pop(); 
+    }
+}
